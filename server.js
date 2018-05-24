390e14f236edadc2d7f9391953c107f9cbbc49ce
Jacques Begin <jacques.andre.begin@gmail.com>
2018-05-24 12:52:29 -0400

    Adds nodemon package to refresh server, adds initial express routes for testing.

--- server.js
+++ server.js
@@ -4,6 +4,25 @@ const app = express();
 const PORT = 8000;
 
 
+
+
+app.route("/").get((req, res) => {
+  console.log("Smoothie Duplicator Home Page");
+  res.send("Smoothie Duplicator Home Page");
+});
+
+app.route("/api/smoothies").get((req, res) => {
+  console.log("Smoothie Duplicator All Smoothies Page");
+  res.send("All Smoothies");
+});
+
+app.route("/api/smoothies/:id").get((req, res) => {
+  console.log("Smoothie Duplicator Single Smoothie Page");
+  res.send("Single Smoothie");
+});
+
+
+
 app.listen(8000, () => {
   console.log(`Server started on port: ${PORT}`);
 });
